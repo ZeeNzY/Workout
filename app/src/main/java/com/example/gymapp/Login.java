4f3630c9f14c7d8c60dcd114016f99744d1f2941
Zeenzy <zinzigulwa@gmail.com>
2021-06-15 14:38:30 +0200

    committed

---
+++ app/src/main/java/com/example/gymapp/Login.java
@@ -0,0 +1,80 @@
+package com.example.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    final int MIN_PASSWORD_LENGTH = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        viewInitializations();
+    }
+
+    void viewInitializations() {
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+        // To show back button in actionbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Checking if the input in form is valid
+    boolean validateInput() {
+
+        if (etEmail.getText().toString().equals("")) {
+            etEmail.setError("Please Enter Email");
+            return false;
+        }
+        if (etPassword.getText().toString().equals("")) {
+            etPassword.setError("Please Enter Password");
+            return false;
+        }
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.getText().toString())) {
+            etEmail.setError("Please Enter Valid Email");
+            return false;
+        }
+
+        // checking minimum password Length
+        if (etPassword.getText().length() < MIN_PASSWORD_LENGTH) {
+            etPassword.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + "characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    // Hook Click Event
+
+    public void performSignUp (View v) {
+        if (validateInput()) {
+
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void goToSignup(View v) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+}
